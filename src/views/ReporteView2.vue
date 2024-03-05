80f99ea29f2108e182658943147aae62cebf904d
juan carlos mejia arguello <juan.mejiar@alumno.buap.mx>
2024-03-05 14:46:01 -0600

    correccion

--- src/views/ReporteView2.vue
+++ src/views/ReporteView2.vue
@@ -64,14 +64,12 @@
 
 <script>
 import axios from "axios";
-import ExcelJS from 'exceljs';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
 
 export default {
   data() {
     return {
       resultados: [],
+      resultadosOriginales: [], // Nueva propiedad para almacenar los datos originales
       idTipo: null,
       estatus: null,
       fechaInicio: null,
@@ -94,8 +92,8 @@ export default {
     axios
       .get("http://189.165.26.146:8081/admin/get.php/transaccionesbanco")
       .then((response) => {
-        this.resultados = response.data.data;
-        console.log(this.resultados);
+        this.resultadosOriginales = response.data.data; // Almacena los datos originales
+        this.resultados = [...this.resultadosOriginales]; // Copia los datos originales a resultados
         this.calcularTotalesPorBanco();
         this.generateChart();
       })
@@ -105,7 +103,7 @@ export default {
   },
   methods: {
     filtrarDatos() {
-      let resultadosFiltrados = this.resultados;
+      let resultadosFiltrados = [...this.resultadosOriginales]; // Comienza con los datos originales
 
       if (this.fechaInicio && this.fechaFin) {
         resultadosFiltrados = resultadosFiltrados.filter(adeudo => {
@@ -125,83 +123,10 @@ export default {
         return totales;
       }, {});
     },
-    downloadPDF() {
-      const pdfOptions = {
-        orientation: "portrait",
-        unit: "mm",
-        format: "letter",
-      };
-
-      const doc = new jsPDF(pdfOptions);
-
-      html2canvas(this.$el, { scale: 3 })
-        .then(canvas => {
-          let imgData = canvas.toDataURL('image/jpeg', 0.1);
-
-          let imgWidth = 200;
-          let imgHeight = (canvas.height * imgWidth) / canvas.width;
-
-          let marginLeft = (doc.internal.pageSize.getWidth() - imgWidth) / 2;
-          let marginTop = 10;
-
-          doc.addImage(imgData, 'JPEG', marginLeft, marginTop, imgWidth, imgHeight);
-
-          doc.save('informe_financiero.pdf');
-        })
-        .catch(error => {
-          console.error('Error al capturar la representación gráfica de la tabla:', error);
-        });
-    },
-    async exportExcel() {
-      const workbook = new ExcelJS.Workbook();
-      const worksheet = workbook.addWorksheet('Sheet1');
-      const tables = this.$el.querySelectorAll('table');
-
-      let rowIndex = 1;
-
-      for (let i = 0; i < tables.length; i++) {
-        const table = tables[i];
-
-        // Convertir la tabla HTML a un array de arrays
-        const data = Array.from(table.querySelectorAll('tr')).map(tr =>
-          Array.from(tr.querySelectorAll('td, th')).map(td => td.innerText)
-        );
-
-        // Agregar los datos a la hoja de Excel
-        data.forEach((row, localRowIndex) => {
-          row.forEach((value, colIndex) => {
-            const cell = worksheet.getCell(rowIndex + localRowIndex, colIndex + 1);
-            cell.value = value;
-
-            // Aplicar negrita a los encabezados de cada columna
-            if (localRowIndex === 0) {
-              cell.font = { bold: true };
-            }
-
-            // Ajustar el ancho de las columnas específicas
-            if (colIndex === 0) {
-              worksheet.getColumn(colIndex + 1).width = 50; // Primera columna
-            } else if (colIndex === 1 || colIndex === 2 || colIndex === 3) {
-              worksheet.getColumn(colIndex + 1).width = 20; // todas las demas columnas
-            }
-          });
-        });
-
-        rowIndex += data.length + 1; // Dejar una fila vacía entre las tablas
-      }
-
-      // Guardar el libro de trabajo como un archivo .xlsx
-      const buffer = await workbook.xlsx.writeBuffer();
-      const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'informe_financieroOKTAN.xlsx';
-      a.click();
-    },
   }
 };
 </script>
+
 
 
 
